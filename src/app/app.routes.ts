a99acedbe8414d1c142b35d0da595897fa1a39a8
matheusssoares <matheusssoares04@gmail.com>
2024-01-21 01:24:09 -0300

    feature: login component add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
 
 export const routes: Routes = [
-    
+    {
+        path: '',
+        pathMatch: 'full',
+        component: LoginComponent,
+        title: 'Paróquia São Joaquim - Login'
+    }
 ];
